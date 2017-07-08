555171225b9ee7ccc139b5304735fcd3e901c65f
Suyleap <shiwuvencii@gmail.com>
2017-07-08 16:32:59 +0700

    i create connection Class

---
+++ StudentInformation/StudentInformation/ConnecttionStudentClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace StudentInformation
+{
+    class ConnecttionStudentClass
+    {
+        public string SQL;
+        public System.Data.SqlClient.SqlConnection CON;
+        public System.Data.SqlClient.SqlCommand CMD;
+        public System.Data.SqlClient.SqlDataReader READER;
+
+       public ConnecttionStudentClass()
+        { }
+
+        public String SQLs
+        {
+            get { return SQL; }
+            set { SQL = value; }
+        }
+
+        public void connection()
+        { 
+
+        }
+
+        public void UseDataBase(string sql)
+        {
+            this.SQLs = sql;
+
+        }
+
+    }
+}
